9f6c115f838a7d5be706c158b1451acf975d4881
StraySpeed <yo2189@naver.com>
2023-06-25 23:56:41 +0900

    Update timeout

--- Cogs/UserManage.py
+++ Cogs/UserManage.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import timedelta
 from discord.ext import commands
 
 class UserManage(commands.Cog):
@@ -21,15 +22,24 @@ class UserManage(commands.Cog):
         await guild.ban(member)
         await ctx.send(f'{member.mention} 님을 밴했습니다.')
 
+    @commands.has_permissions(moderate_members=True)
+    @commands.hybrid_command(name="타임아웃", with_app_command=True)
+    async def timeout(self, ctx: commands.Context, member: discord.Member, *, time: int = 60):
+        """ 해당 유저를 TIME초 동안 타임아웃합니다. (기본 1분) """
+        await member.timeout(timedelta(seconds=time))
+        await ctx.send(f'{member.mention} 님을 {time}초 동안 타임아웃했습니다.')
+
     @kick.error
     @ban.error
+    @timeout.error
     async def user_manage_error(self, ctx: commands.Context, error: commands.errors):
         if isinstance(error, commands.errors.MissingPermissions):
             await ctx.send(f"{ctx.author.mention}님, 해당 권한이 없습니다.")
         if isinstance(error, commands.errors.HybridCommandError):   # hybrid command error
             await ctx.send(f"{ctx.author.mention}님, 해당 명령어를 사용할 수 없습니다.")
-        if isinstance(error, commands.errors.MissingRequiredArgument):  # prefix command error
+        if isinstance(error, commands.errors.MissingRequiredArgument) or isinstance(error, commands.errors.BadArgument):  # prefix command error
             await ctx.send(f"{ctx.author.mention}님, 명령어를 정확히 입력해 주세요.")
+
 
 async def setup(bot): # Cog를 추가하는 코루틴
     await bot.add_cog(UserManage(bot)) # add the cog to the bot
