2533f05aed835401dc8ef0d1e69d3a49f42f0530
StraySpeed <yo2189@naver.com>
2023-09-17 23:35:25 +0900

    add selfkick method

--- Cogs/UserManage.py
+++ Cogs/UserManage.py
@@ -29,16 +29,27 @@ class UserManage(commands.Cog):
         await member.timeout(timedelta(seconds=time))
         await ctx.send(f'{member.mention} 님을 {time}초 동안 타임아웃했습니다.')
 
+    @commands.command(name="명명")
+    async def self_kick(self, ctx: commands.Context):
+        """ 자기 자신을 강퇴시킵니다. """
+        guild = ctx.guild
+        await guild.kick(ctx.author)
+        await ctx.send("**어휴**")
+
+    @self_kick.error
     @kick.error
     @ban.error
     @timeout.error
     async def user_manage_error(self, ctx: commands.Context, error: commands.errors):
         if isinstance(error, commands.errors.MissingPermissions):
             await ctx.send(f"{ctx.author.mention}님, 해당 권한이 없습니다.")
-        if isinstance(error, commands.errors.HybridCommandError):   # hybrid command error
+        elif isinstance(error, commands.errors.HybridCommandError):   # hybrid command error
             await ctx.send(f"{ctx.author.mention}님, 해당 명령어를 사용할 수 없습니다.")
-        if isinstance(error, commands.errors.MissingRequiredArgument) or isinstance(error, commands.errors.BadArgument):  # prefix command error
+        elif isinstance(error, commands.errors.MissingRequiredArgument) or isinstance(error, commands.errors.BadArgument):  # prefix command error
             await ctx.send(f"{ctx.author.mention}님, 명령어를 정확히 입력해 주세요.")
+        else:
+            await ctx.send(f"오류가 발생했습니다.")
+            await ctx.send(f"{error.original}")
 
 
 async def setup(bot): # Cog를 추가하는 코루틴
